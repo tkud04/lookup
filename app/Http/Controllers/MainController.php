cda7c4da2c9338c6fc226baca6d2007a18041988
Rich Mama <postmaster.richmama@gmail.com>
2018-02-07 13:56:29 +0100

    Changed from curl to file get contents

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -49,39 +49,19 @@ class MainController extends Controller {
                  else
                  { 
                  	 $phonenums = $req["phonenums"];
-	$arr = explode("\n", $phonenums);
-	$ret = array();
+	                  $arr = explode("\n", $phonenums);
+	                   $ret = array();
 	
-	foreach($arr as $phone){
-	$npa = substr($phone,0,3); $nxx = substr($phone,3,3); $thou = substr($phone,-4);
-	$result = "";
-	$url = "http://www.fonefinder.net/findome.php?npa=".$npa."&nxx=".$nxx."&thoublock=".$thou."&usaquerytype=Search+by+Number&cityname=";
-	#echo "url is ". $url."<br><br>";
-	
-    $ch = curl_init();
-
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPGET, 1);
-curl_setopt($ch, CURLOPT_URL, $url );
-curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, false );
-curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 2 );
-curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
-
-   
-    $result = curl_exec($ch);
+	                   foreach($arr as $phone){
+	                      $npa = substr($phone,0,3); $nxx = substr($phone,3,3); $thou = substr($phone,-4);
+	                      $result = "";
+	                      $url = "http://www.fonefinder.net/findome.php?npa=".$npa."&nxx=".$nxx."&thoublock=".$thou."&usaquerytype=Search+by+Number&cityname=";
+	                      #echo "url is ". $url."<br><br>";
+	                      $result = file_get_contents($url);    
+                          array_push($ret, $result);
+                      } 
     
-    if (curl_errno ( $ch )) {
-    echo curl_error ( $ch );
-    curl_close ( $ch );
-    exit();
-    }
-    
-    array_push($ret, $result);
-   curl_close ( $ch );
-   } 
-    
-    return json_encode($ret);                       
+                       return json_encode($ret);                       
                    }                                                                                                   
 	}
 
