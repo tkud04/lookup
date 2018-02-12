2b9ca889706b7ce89a949370bde3920c8812a0f8
Rich Mama <postmaster.richmama@gmail.com>
2018-02-12 16:06:36 +0100

    Stristr

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -220,7 +220,8 @@
                     
                        if(carrier != ""){
                           	console.log(carrier);
-                              if(carrier in ss){
+                              for(var key in ss){
+                              	if(stristr(carrier, key) != false){
                                   gwcode = ss[carrier];
                                   gwsyntax = raw[gwcode];
                                   
@@ -233,6 +234,7 @@
                                   td = $('<td></td>');
                                   td.append(gbana);
                                   tr.append(td);
+                                } 
       		                } 
                           }               	    
                      
@@ -252,6 +254,33 @@
         
     });
 
+   </script>
+   <script>
+   	function stristr(haystack, needle, bool) {
+//  discuss at: http://locutus.io/php/stristr/
+// original by: Kevin van Zonneveld (http://kvz.io)
+// bugfixed by: Onno Marsman (https://twitter.com/onnomarsman)
+//   example 1: stristr('Kevin van Zonneveld', 'Van')
+//   returns 1: 'van Zonneveld'
+//   example 2: stristr('Kevin van Zonneveld', 'VAN', true)
+//   returns 2: 'Kevin '
+var pos = 0;
+haystack += '';
+pos = haystack.toLowerCase().indexOf((needle + '').toLowerCase ());
+
+if (pos === - 1) {
+return false;
+} 
+
+else {
+if (bool) {
+return haystack. substr(0 , pos);
+} 
+else {
+return haystack.slice(pos);
+}
+}
+}
    </script>
 
 </body>
